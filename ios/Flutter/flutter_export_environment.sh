7f7673eba891687ef1527ebb0513d443334a865f
Juan Vallejo <juan_vallejo@uri.edu>
2025-10-05 02:01:33 -0400

    chore(lints): apply analyzer fixes and format

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/sdks/flutter"
-export "FLUTTER_APPLICATION_PATH=/app"
+export "FLUTTER_ROOT=C:\Users\valle\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\valle\Desktop\90\Sierra-Painting-v1"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.12"
 export "FLUTTER_BUILD_NUMBER=12"
